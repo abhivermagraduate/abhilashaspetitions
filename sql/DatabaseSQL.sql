469b8f9a81a65a77268ff5d96ae3cf996a49b766
Abhilasha Verma <abhilashaverma564@gmail.com>
2023-11-12 10:06:35 +0000

    Added SQL File and updated DB password

---
+++ sql/DatabaseSQL.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v12.09 (64 bit)
+MySQL - 8.0.31 : Database - petitionmanagement
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`petitionmanagement` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `petitionmanagement`;
+
+/*Table structure for table `petition` */
+
+DROP TABLE IF EXISTS `petition`;
+
+CREATE TABLE `petition` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `petition_text` blob,
+  `create_date_time` timestamp NULL DEFAULT NULL,
+  `petition_scope` varchar(255) DEFAULT NULL,
+  `geographic_area` varchar(255) DEFAULT NULL,
+  `petition_title` varchar(255) DEFAULT NULL,
+  `image_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
